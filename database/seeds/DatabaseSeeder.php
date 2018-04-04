67d64315bb85fddb2edd27f41b330de07a4f5e4e
Felipe Braiani <felipe.brainitech@gmail.com>
2018-04-04 13:39:06 -0400

    Ajustado ordem de Seed das tabelas de acordo com as ForeignKeys

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,17 +11,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $this->call(DataRowsTableSeeder::class);
-        $this->call(DataTypesTableSeeder::class);
         $this->call(MenusTableSeeder::class);
         $this->call(MenuItemsTableSeeder::class);
         // $this->call(MigrationsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
         $this->call(PermissionRoleTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
         $this->call(SettingsTableSeeder::class);
         $this->call(StatusTableSeeder::class);
         $this->call(UserRolesTableSeeder::class);
+        $this->call(DataTypesTableSeeder::class);
+        $this->call(DataRowsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
