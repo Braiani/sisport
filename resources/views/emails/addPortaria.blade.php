1ebe69587b9951431061fef4d9023b5a4191533b
Felipe Braiani <felipe.braianitech@gmail.com>
2018-08-27 06:43:01 -0400

    Start working on Email notifications.

---
+++ resources/views/emails/addPortaria.blade.php
@@ -0,0 +1,6 @@
+<p>Prezados,</p>
+<p>boa tarde!</p>
+<br><br>
+<p>Encaminhamos ​Portaria nº 81_2018_ Comissão de organização do Processo Seletivo Simplificado - PSS_Edital 38_2018_Rev. Port. 72_2018, para conhecimento da inclusão de membro na comissão organizadora do PSS.</p>
+<br>
+<p>At.te,</p>
