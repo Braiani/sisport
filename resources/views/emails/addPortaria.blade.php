e09961bdaba0134b73831d4c44791b2620b3c120
Felipe Braiani <felipe.braianitech@gmail.com>
2018-12-13 00:06:28 -0300

    E-mail model updated

--- resources/views/emails/addPortaria.blade.php
+++ resources/views/emails/addPortaria.blade.php
@@ -1,44 +1,45 @@
-<img src="https://ci4.googleusercontent.com/proxy/jnX5n3rOQzAfgFw13WxY9eHYwtS0vAKZskFowh7-36w86oftNCbcRn9tZN3K5Ls8xuuuFuOk0vFNPe5j3gnsDey5daOYFPI3H9MlcVZSvSCdfBEYVmYFmX1N-fqGHPdl35ViG1FsSrrexCcYBtRVeb91PxKxA5-rzL5xgOivKO16Njgke_YqFAeYNtiGVNpyuEJyy7m4Lyo-sUUf0387TDyLyOCGHdZYo00qgVFLPakSgFeohWZT=s0-d-e1-ft#http://servidor.ifms.edu.br/publicacoes/identidade-visual/cabecalhos-de-e-mails-institucionais/copy13_of_e-mail_gabin.png/@@download/file/e-mail_c_campo-grande.png" class="CToWUd" width="420" height="61"><br>
-<p>Prezados,</p>
-<p>Informamos que a Portaria nº {{ $portaria->port_num }} - {{ $portaria->descricao }} foi encaminhada para publicação.</p>
-
-<p><u><b>Importante:</b>  Fiquem atentos aos prazos de atividades, elaboração de plano de trabalho, relatórios e validade da portaria.</u></p>
-
-<p>
-	Informamos que os modelos de relatórios encontram-se no suap e deverão ser encaminhados à Dirge pelo  Presidente/Coordenador
-	da Comissão por meio do <b>Processo eletrônico que consta na referida Portaria.</b> Neste processo eletrônico, deverá constar o relatório do  
-	Presidente/Coordenador , bem como os relatórios individuais dos demais membros.
-</p>
-
-<p>Para acessar o modelo de relatório, acesse o <u>SUAP> ADMINISTRAÇÃO>Documentos Eletrônicos>Documentos>Adicionar Documento de Texto</u></p>
-<p>Tipo de Documento: <b>Relatório.</b></p>
-<p>Modelo de Documento:  <b>Relatório Final das Atividades Desenvolvidas pela Comissão (preenchido pelo Presidente/Coordenador)</b></p>
-<p>Modelo de Documento:  <b>Relatório Individual de Atividades Desenvolvidas em Comissão (preenchido pelos demais membros).</b></p>
-
-<p>At.te,</p>
-<div>
-	<font size="1" face="verdana, sans-serif" color="#274e13">Fernanda Freitas</font>
-</div>
-<div style="font-size:small">
-<div>
-	<font size="1" face="verdana, sans-serif" color="#274e13">
-		<b>Equipe Direção-geral&nbsp;</b>
-		<div style="display:inline"><b>do</b>&nbsp;</div>
-		<strong><i>Campus&nbsp;</i>Campo Grande</strong>
-	</font>
-</div>
-<p style="margin-top:0px;line-height:15.6px">
-	<font size="1" color="#274e13">
-		<font face="verdana, sans-serif">Rua Taquari nº 831,&nbsp;Santo Antônio,&nbsp;Campo Grande/MS&nbsp;79100-510&nbsp;
-			<br>(67) 3357-850<span class="gmail_default" style="font-family:verdana,sans-serif">4</span>&nbsp;
-		</font>
-		<span style="font-family:verdana,sans-serif">•</span>
-		<font face="verdana, sans-serif">&nbsp;
-			<a href="http://www.ifms.edu.br/" style="color:rgb(17,85,204)" target="_blank" >Site</a>
-				&nbsp;•&nbsp;
-			<a href="http://facebook.com/ifms.cg" style="color:rgb(17,85,204)" target="_blank" >Facebook</a>
-				&nbsp;•&nbsp;
-			<a href="http://youtube.com/ifmscomunica" style="color:rgb(17,85,204)" target="_blank"><wbr>Youtube</a>
-		</font>
-	</font>
-</p>
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+	<meta charset="UTF-8">
+	<title>Portaria nº {{ $portaria->port_num }} - {{ $portaria->descricao }}</title>
+</head>
+<body>
+	<img src="{{ asset('img/email-head.png') }}" width="420" height="61"><br>
+	<p>Prezados,</p>
+	<p>Informamos que a Portaria nº {{ $portaria->port_num }} - {{ $portaria->descricao }} foi encaminhada para publicação.</p>
+	
+	<p><u><b>Importante:</b>  Fiquem atentos aos prazos de atividades, elaboração de plano de trabalho, relatórios e validade da portaria.</u></p>
+	
+	<p>
+		Informamos que os modelos de relatórios encontram-se no suap e deverão ser encaminhados à Dirge pelo  Presidente/Coordenador
+		da Comissão por meio do <b>Processo eletrônico que consta na referida Portaria.</b> Neste processo eletrônico, deverá constar o relatório do  
+		Presidente/Coordenador , bem como os relatórios individuais dos demais membros.
+	</p>
+	
+	<p>Para acessar o modelo de relatório, acesse o <u>SUAP > ADMINISTRAÇÃO > Documentos Eletrônicos > Documentos > Adicionar Documento de Texto</u></p>
+	<p>Tipo de Documento: <b>Relatório.</b></p>
+	<p>Modelo de Documento:  <b>Relatório Final das Atividades Desenvolvidas pela Comissão (preenchido pelo Presidente/Coordenador)</b></p>
+	<p>Modelo de Documento:  <b>Relatório Individual de Atividades Desenvolvidas em Comissão (preenchido pelos demais membros).</b></p>
+	
+	<p>At.te,</p>
+	
+	<div style="color: #274e13; font-family: Verdana, sans-serif; font-size: small; margin-top:0px;line-height:5px">
+		<p>
+			{{ setting("admin.title") }} - {{ setting("admin.description") }}
+		</p>
+		<p>
+			<b>Equipe Direção-geral do <i>Campus</i> Campo Grande</b>
+		</p>
+		<p>
+			Rua Taquari nº 831, Santo Antônio, Campo Grande/MS 79100-510
+		</p>
+		<p>
+			(67) 3357-8504 • 
+			<a href="http://www.ifms.edu.br/" target="_blank" >Site</a> •
+			<a href="http://facebook.com/ifms.cg" target="_blank" >Facebook</a> •
+			<a href="http://youtube.com/ifmscomunica" target="_blank">Youtube</a>
+		</p>
+	</div>
+</body>
+</html>
