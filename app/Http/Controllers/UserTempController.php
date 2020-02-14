b3167215387d025d1882243cbfca66d930987ad0
Felipe Braiani <felipe.braianitech@gmail.com>
2020-02-14 00:26:02 -0400

    Implemented a way to add user

--- app/Http/Controllers/UserTempController.php
+++ app/Http/Controllers/UserTempController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Pessoa;
 use App\User;
 use Illuminate\Http\Request;
 use TCG\Voyager\Facades\Voyager;
-use TCG\Voyager\Http\Controllers\VoyagerBaseController;
+use TCG\Voyager\Http\Controllers\VoyagerUserController;
 
-class UserTempController extends VoyagerBaseController
+class UserTempController extends VoyagerUserController
 {
     public function index(Request $request)
     {
@@ -20,11 +21,21 @@ class UserTempController extends VoyagerBaseController
         }
     }
 
+    public function store(Request $request)
+    {
+        $request->merge([
+            'alter_pass' => $request->alter_pass ? true : false,
+            'pessoa_id' => $request->person,
+        ]);
+        return parent::store($request);
+    }
+
+
     public function update(Request $request, $id)
     {
         $user = User::findOrFail($id);
         $request->merge([
-            'alter_pass' => $user->alter_pass,
+            'alter_pass' => $request->alter_pass ? true : false,
             'siape' => $user->siape,
             'pessoa_id' => $user->pessoa_id,
             'role_id' => $user->role_id
@@ -32,5 +43,27 @@ class UserTempController extends VoyagerBaseController
         return parent::update($request, $id);
     }
 
+    public function searchPeople(Request $request)
+    {
+        $search = $request->term ?? null;
 
+        $people = Pessoa::when(!is_null($search), function ($query) use ($search) {
+            $query = $query->where('nome', "LIKE", "%{$search}%");
+            $query = $query->orWhere('siape', "LIKE", "%{$search}%");
+            $query = $query->orWhere('email', "LIKE", "%{$search}%");
+
+            return $query->get();
+        }, function ($query) {
+            return $query->limit(15)->get();
+        });
+
+        return [
+            "results" => $people
+        ];
+    }
+
+    public function searchPerson(Pessoa $person)
+    {
+        return $person->load('usuario');
+    }
 }
